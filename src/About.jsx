eaa5292b82ab39bd65f29c70ae6c2bf80d511e69
Jon <orejajonathan@gmail.com>
2020-07-06 16:00:02 +0200

    css fix

--- src/About.jsx
+++ src/About.jsx
@@ -8,8 +8,8 @@ function About() {
 
             <p id="text">
        
-            Diplômé (septembre 2017) du programme Grande Ecole de <span>Grenoble Ecole de Management.</span> J'ai commencé ma carrière sur des postes commerciaux et de gestion de projet dans le milieu des starts up. En janvier 2020, J'ai entamé une <span>reconversion professionnelle pour devenir Web Developer</span>en suivant le bootcamp d'<span>Ironhack</span>.<br/> <br/>
-            Je suis donc <span>à la recherche d'un poste en tant que Web Developer Fullstack Junior </span>dans une société qui pourra continuer à me faire progresser sur ce domaine. 
+            Diplômé (septembre 2017) du programme Grande Ecole de <span>Grenoble Ecole de Management.</span> J'ai commencé ma carrière sur des postes commerciaux et de gestion de projet dans le milieu des starts up. En janvier 2020, J'ai entamé une <span>reconversion professionnelle pour devenir developpeur web </span>en suivant le bootcamp d'<span>Ironhack</span>.<br/> <br/>
+            Je suis donc <span>à la recherche d'un poste en tant que Web Developer Fullstack Junior </span>dans une société qui pourra continuer à me faire progresser dans ce domaine. 
             </p>
             <Contact/>
            

b428c226c1d5d3d8399a67c8c09d1f32d1b4b5fe
Jon <orejajonathan@gmail.com>
2020-07-06 15:51:29 +0200

    link

--- src/Projets.jsx
+++ src/Projets.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import {  Link } from 'react-router-dom';
+
 
 function Projets() {
     return (
         <div  id="wrapper_projets">
             <div className="animate__animated animate__bounceIn" id="tetris" className="projet">
+                <a href="https://tetris-app-with-react.herokuapp.com/">
                 <h2>Tetris</h2>
+                </a>
             </div>
             <div className="animate__animated animate__bounceIn" id="client" className="projet">
                 <h2>
@@ -12,23 +16,34 @@ function Projets() {
                 </h2>
             </div>
             <div className="animate__animated animate__bounceIn" id="pendu" className="projet">
+                <a href="https://jeu-du-pendu-react.herokuapp.com">
                 <h2>
                     Le Jeu du Pendu
                 </h2>
+                </a>
             </div>
             <div className="animate__animated animate__bounceIn" id="vintage" className="projet">
+                <a href="https://book-photo.herokuapp.com">
                 <h2>
                 My Vintage Trips
                 </h2>
+                </a>
             </div>
             <div className="animate__animated animate__bounceIn" id="ollie" className="projet">
+                <a href="https://ollie-front.herokuapp.com">
                 <h2>Ollie</h2>
+                </a>
+                
             </div>
             <div className="animate__animated animate__bounceIn" id="cliché" className="projet">
+                <a href="https://cliche-app.herokuapp.com">
                 <h2>Cliché</h2>
+                </a>
             </div>
             <div className="animate__animated animate__bounceIn" id="mordor" className="projet">
+                <a href="https://jarchiii.github.io/Lotr-projectGame">
                 <h2>Journey to Mordor</h2>
+                </a>
             </div>
             
         </div>

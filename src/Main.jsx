d10701cc54c3d07c38bff2932e85538eb1899e47
Jon <orejajonathan@gmail.com>
2020-07-06 12:46:08 +0200

    more style

--- src/Main.jsx
+++ src/Main.jsx
@@ -7,6 +7,7 @@ import About from "./About"
 import Contact from "./Contact"
 import Projets from "./Projets"
 import Intro from "./Intro"
+import { HashLink as Link } from 'react-router-hash-link';
 
 function Main() {
     return (
@@ -17,7 +18,15 @@ function Main() {
 
         <section id="section0">
             <div className="animate__animated animate__bounceInRight" id="hello">
-                Bonjour, je m'appelle Jonathan Oreja.
+                <p>Hello !</p>
+            
+            <Link to="#section1">
+
+            <div className="menu">-Mes Projets</div>
+            </Link>
+            <Link to="#section2">
+            <div className="menu">-A propos de moi</div>
+            </Link>
             </div>
         </section>
         
@@ -36,7 +45,7 @@ function Main() {
       <WhyMe/>
       <section/> */}
       <footer>
-          <h5>Jonathan Oreja  ©</h5>
+          <h5>Jonathan Oreja  © 2020</h5>
       </footer>
 
         </div>
